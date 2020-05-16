cf399a5f847c832c027621ac83d700ac6979e912
B.A. Imambaks Rodrigues <0924777@hr.nl>
2020-05-16 13:06:27 +0200

    played around with svg

--- crashcourse/index.js
+++ crashcourse/index.js
@@ -6,16 +6,15 @@
 // var material3;
 // var cube;
 // var cloudGeo, cloudMaterial
-// let cloud; 
-
+// let cloud;
 
 // function init() {
 
 //   // New scene
 //   scene = new THREE.Scene();
 
-//   // Scene configs 
-//   scene.fog = new THREE.FogExp2(0x03544e, 0.001); 
+//   // Scene configs
+//   scene.fog = new THREE.FogExp2(0x03544e, 0.001);
 
 //   // Camera position;
 //   camera = new THREE.PerspectiveCamera(
@@ -24,12 +23,10 @@
 //     1,
 //     1000
 //   );
-//     camera.position.z = 1; 
-//     camera.rotation.x = 1.16; 
-//     camera.rotation.y = -0.12; 
-//     camera.rotation.z = 0.27; 
-  
-
+//     camera.position.z = 1;
+//     camera.rotation.x = 1.16;
+//     camera.rotation.y = -0.12;
+//     camera.rotation.z = 0.27;
 
 //   // Responsive resizing
 //   window.addEventListener("resize", function() {
@@ -48,19 +45,18 @@
 //   var divisions = 10;
 //   document.body.appendChild(renderer.domElement);
 
-
 //   // Controls
 //   // controls = new THREE.OrbitControls( camera, renderer.domElement );
 
 //   // Light
 //   // var light = new THREE.PointLight(0xffffff, 1, 500);
 //   // light.position.set(20, 30, 20);
-//   let ambient = new THREE.AmbientLight(0x555555); 
-//   scene.add(ambient); 
-  
-//   let directionalLight = new THREE.DirectionalLight(0xff8c19); 
-//   directionalLight.position.set(0, 0, 1); 
-//   scene.add(directionalLight); 
+//   let ambient = new THREE.AmbientLight(0x555555);
+//   scene.add(ambient);
+
+//   let directionalLight = new THREE.DirectionalLight(0xff8c19);
+//   directionalLight.position.set(0, 0, 1);
+//   scene.add(directionalLight);
 
 //   let orangeLight = new THREE.PointLight(0xcc6600,50,450,1.7);
 //   orangeLight.position.set(200,300,100);
@@ -72,8 +68,7 @@
 //   blueLight.position.set(300,300,200);
 //   scene.add(blueLight);
 
-
-//   // Load texture 
+//   // Load texture
 //   // instantiate a loader
 // var loader = new THREE.TextureLoader();
 
@@ -88,26 +83,26 @@
 // 			map: texture
 //      });
 //     });
-  
-// //   let loader = new THREE.TextureLoader(); 
-// //   loader.load('./img/smoke.png', function(texture) { 
-// //     cloudGeo = new THREE.PlaneBufferGeometry(500, 500); 
-// //     cloudMaterial = new THREE.MeshLambertMaterial({ 
-// //       map: texture, 
+
+// //   let loader = new THREE.TextureLoader();
+// //   loader.load('./img/smoke.png', function(texture) {
+// //     cloudGeo = new THREE.PlaneBufferGeometry(500, 500);
+// //     cloudMaterial = new THREE.MeshLambertMaterial({
+// //       map: texture,
 // //       transparent: true
-// //   }); 
-// // }); 
+// //   });
+// // });
 
 // for(let p = 0; p<50; p++) {
-//   cloud = new THREE.Mesh(cloudGeo, cloudMaterial); 
+//   cloud = new THREE.Mesh(cloudGeo, cloudMaterial);
 //   cloud.position.set(
 //     Math.random()*800 - 400, 500, Math.random()*500-500
-//   ); 
-//   cloud.rotation.x = 1.16; 
-//   cloud.rotation.y = -0.12; 
-//   cloud.rotation.z = Math.random()*2*Math.PI; 
-//   cloud.material.opacity = 0.55; 
-//   scene.add(cloud); 
+//   );
+//   cloud.rotation.x = 1.16;
+//   cloud.rotation.y = -0.12;
+//   cloud.rotation.z = Math.random()*2*Math.PI;
+//   cloud.material.opacity = 0.55;
+//   scene.add(cloud);
 //   console.log(cloud);
 // }
 
@@ -115,7 +110,6 @@
 //   var geometry2 = new THREE.BoxGeometry(1, 80, );
 //   var material2 = new THREE.MeshLambertMaterial({ color: 0xffffff });
 //   mesh2 = new THREE.Mesh(geometry2, material2);
-  
 
 //   // create an AudioListener and add it to the camera
 //   var listener = new THREE.AudioListener();
@@ -131,7 +125,6 @@
 //     sound.setVolume(0.5);
 //     sound.play();
 //   });
-
 
 //   // Add objects to scene
 //   // scene.add(mesh2);
@@ -157,18 +150,38 @@
 // // var t1 = gsap.timeline().delay(2);
 // // this.t1.to(this.mesh2.position, 1, { y: 2, ease: Expo.easeOut });
 
+let scene, camera;
+let cloudParticlesRight = [];
+let cloudParticlesLeft = [];
+let url =
+  "https://api.breezometer.com/air-quality/v2/current-conditions?lat=34.0326633&lon=-118.2111158&key=4b1108d891904f8f8582a170a552a47a&features=breezometer_aqi,local_aqi,sources_and_effects";
 
-let scene, camera, cloudParticles = []
+fetch(url)
+  .then(response => {
+    return response.json();
+  })
+  .then(data => {
+    console.log(data.data.indexes.baqi.aqi);
+    init(data.data.indexes.baqi.aqi);
+  })
+  .catch(e => {
+    console.log("Error occured." + e);
+  });
 
 function init(cloudIndex) {
   scene = new THREE.Scene();
-  camera = new THREE.PerspectiveCamera(60,window.innerWidth / window.innerHeight,1,1000);
+  camera = new THREE.PerspectiveCamera(
+    60,
+    window.innerWidth / window.innerHeight,
+    1,
+    1000
+  );
   camera.position.z = 1;
   camera.rotation.x = 1.16;
-  camera.rotation.y = -0.12;
+  camera.rotation.y = -0.13;
   camera.rotation.z = 0.27;
 
-    // Responsive resizing
+  // Responsive resizing
   window.addEventListener("resize", function() {
     var WIDTH = window.innerWidth;
     var HEIGHT = window.innerHeight;
@@ -181,96 +194,97 @@ function init(cloudIndex) {
   scene.add(ambient);
 
   let directionalLight = new THREE.DirectionalLight(0xff8c19);
-  directionalLight.position.set(0,0,1);
+  directionalLight.position.set(0, 0, 1);
   scene.add(directionalLight);
 
-  let orangeLight = new THREE.PointLight(0x406340,50,450,1.7);
-  orangeLight.position.set(200,300,100);
+  let orangeLight = new THREE.PointLight(0x406340, 50, 450, 1.7);
+  orangeLight.position.set(200, 300, 100);
   scene.add(orangeLight);
-  let redLight = new THREE.PointLight(0x293029,50,450,1.7);
-  redLight.position.set(100,300,100);
+  let redLight = new THREE.PointLight(0x293029, 50, 450, 1.7);
+  redLight.position.set(100, 300, 100);
   scene.add(redLight);
-  let blueLight = new THREE.PointLight(0x182918,50,450,1.7);
-  blueLight.position.set(300,300,200);
+  let blueLight = new THREE.PointLight(0x182918, 50, 450, 1.7);
+  blueLight.position.set(300, 300, 200);
   scene.add(blueLight);
 
   renderer = new THREE.WebGLRenderer();
-  renderer.setSize(window.innerWidth,window.innerHeight);
+  renderer.setSize(window.innerWidth, window.innerHeight);
   scene.fog = new THREE.FogExp2(0x262626, 0.001);
   renderer.setClearColor(scene.fog.color);
   document.body.appendChild(renderer.domElement);
 
+  let material = new THREE.MeshBasicMaterial();
+
   let loader = new THREE.TextureLoader();
-  loader.load("img/smoke.png", function(texture){
-    cloudGeo = new THREE.PlaneBufferGeometry(500,500);
+  loader.load("img/smoke.png", function(texture) {
+    cloudGeo = new THREE.PlaneBufferGeometry(500, 500);
     cloudMaterial = new THREE.MeshLambertMaterial({
-      map:texture,
+      map: texture,
       transparent: true
     });
 
-   
-      for(let i = 0; i < cloudIndex; i++) { 
+    for (let i = 0; i < Math.floor(cloudIndex / 2); i++) {
       let cloud = new THREE.Mesh(cloudGeo, cloudMaterial);
-      cloud.position.set(
-        Math.random()*800 -400,
-        500,
-        Math.random()*500-500
-      );
+      cloud.position.set(Math.random() * 3000, 500, Math.random() * 500 - 500);
       cloud.rotation.x = 1.16;
       cloud.rotation.y = -0.12;
-      cloud.rotation.z = Math.random()*2*Math.PI;
-      cloud.material.opacity = 0.55;
-      cloudParticles.push(cloud);
+      cloud.rotation.z = Math.random() * 2 * Math.PI;
+      cloud.material.opacity = 0.3;
+      cloudParticlesRight.push(cloud);
       scene.add(cloud);
-      console.log
-      console.log(cloud);
-      }
+    }
+    0;
+
+    for (let j = 0; j < Math.floor(cloudIndex / 2); j++) {
+      let cloud = new THREE.Mesh(cloudGeo, cloudMaterial);
+      cloud.position.set(Math.random() * -1200, 300, Math.random() * 500 - 500);
+      cloud.rotation.x = 1.18;
+      cloud.rotation.y = -0.12;
+      cloud.rotation.z = Math.random() * 2 * Math.PI;
+      cloud.material.opacity = 0.3;
+      cloudParticlesLeft.push(cloud);
+      scene.add(cloud);
+    }
   });
 
-    render();
+  // create an AudioListener and add it to the camera
+  var listener = new THREE.AudioListener();
+  camera.add(listener);
+  // create a global audio source
+  var sound = new THREE.Audio(listener);
+
+  // load a sound and set it as the Audio object's buffer
+  var audioLoader = new THREE.AudioLoader();
+  audioLoader.load("./sounds/ambient.mp3", function(buffer) {
+    sound.setBuffer(buffer);
+    sound.setLoop(true);
+    sound.setVolume(0.2);
+    sound.play();
+  });
+
+  render();
 }
 
-
 function render() {
-  cloudParticles.forEach(p => {
-    p.rotation.z -=0.001;
-    
-    if(p.position.x > 200|| p.position.y > 200) { 
-      p.position.x = 0; 
-    } else { 
-      p.position.x =+ Math.random() * 200
+  cloudParticlesRight.forEach(p => {
+    p.rotation.z -= 0.001;
+    p.position.x -= Math.random() * 0.5;
+
+    if (p.position.x < -1000) {
+      p.position.x = 1300;
+      p.position.x -= Math.random() * 0.5;
+    }
+  });
+
+  cloudParticlesLeft.forEach(p => {
+    p.rotation.z -= 0.0001;
+    p.position.x += Math.random() * 0.5;
+
+    if (p.position.x > 1000) {
+      p.position.x = -1300;
+      p.position.x = Math.random() * 0.5;
     }
   });
   requestAnimationFrame(render);
   renderer.render(scene, camera);
 }
-
-// let apiData; 
-// let apiCountry; 
-
-// fetch('https://api.airvisual.com/v2/countries?key=2459f634-caa6-4274-9ce4-b1ddc4399580')
-// .then(response => { 
-//   return response.json();
-// }).then(users => { 
-//   apiData = users.data;
-//   for(let i = 0; i<apiData.length; i++) { 
-//     if(apiData[i].country == "USA") { 
-//       apiCountry = apiData[i].country;
-//       console.log(apiCountry);
-//     }
-//   }
-// }).catch((error) => { 
-//   console.log(error);
-// })
-
-let url = 'https://api.breezometer.com/air-quality/v2/current-conditions?lat=34.0326633&lon=-118.2111158&key=4b1108d891904f8f8582a170a552a47a&features=breezometer_aqi,local_aqi,sources_and_effects';
-
-fetch(url)
-.then(response => { 
-  return response.json(); 
-}).then(data => { 
-  console.log(data.data.indexes.baqi.aqi);
-  init(data.data.indexes.baqi.aqi);
-}).catch((e) => { 
-  console.log("Error occured." + e); 
-})
